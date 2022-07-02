1e32d6da45e07e2c25c36d7ceda52e1852d3c94f
anh le <anhln@gmail.com>
2022-07-02 10:12:50 +0700

    near scold

--- test_calculator.py
+++ test_calculator.py
@@ -7,13 +7,12 @@ import calculator
 
 class TestCalculator:
 
-
     def test_addition(self):
         assert 4 == calculator.add(2, 2)
 
-
     def test_subtraction(self):
         assert 4 == calculator.subtract(6, 2)
+        
 
 
 
