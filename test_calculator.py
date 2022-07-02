82c3c20d9a9d03c666ba359011fd559c5fe68b9f
anh le <anhln@gmail.com>
2022-07-02 10:10:35 +0700

    autopep8 --select=w293

--- test_calculator.py
+++ test_calculator.py
@@ -4,13 +4,16 @@ Unit test for calculator
 
 import calculator
 
+
 class TestCalculator:
-    
+
+
     def test_addition(self):
         assert 4 == calculator.add(2, 2)
-        
-        
+
+
     def test_subtraction(self):
         assert 4 == calculator.subtract(6, 2)
-        
-    
+
+
+
