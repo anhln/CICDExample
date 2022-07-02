c395e9595056c78f38f8f70dc54b165fe6d350bb
anh le <anhln@gmail.com>
2022-07-02 10:14:17 +0700

    .

--- test_calculator.py
+++ test_calculator.py
@@ -9,10 +9,7 @@ class TestCalculator:
 
     def test_addition(self):
         assert 4 == calculator.add(2, 2)
-
+        
     def test_subtraction(self):
         assert 4 == calculator.subtract(6, 2)
         
-
-
-
